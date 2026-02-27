5077fd15be1c150ea6d38ac7308fafc3751280ee
mmgamal <medhat_gamal@icloud.com>
2026-02-27 21:50:56 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -6,32 +6,32 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-  env.APPDATA !== undefined && env.APPDATA !== ''
-    ? `${env.APPDATA}/ASP.NET/https`
-    : `${env.HOME}/.aspnet/https`;
+// const baseFolder =
+//   env.APPDATA !== undefined && env.APPDATA !== ''
+//     ? `${env.APPDATA}/ASP.NET/https`
+//     : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "offline-client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+// const certificateName = "offline-client";
+// const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+// const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(baseFolder)) {
-  fs.mkdirSync(baseFolder, { recursive: true });
-}
+// if (!fs.existsSync(baseFolder)) {
+//   fs.mkdirSync(baseFolder, { recursive: true });
+// }
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  if (0 !== child_process.spawnSync('dotnet', [
-    'dev-certs',
-    'https',
-    '--export-path',
-    certFilePath,
-    '--format',
-    'Pem',
-    '--no-password',
-  ], { stdio: 'inherit', }).status) {
-    throw new Error("Could not create certificate.");
-  }
-}
+// if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//   if (0 !== child_process.spawnSync('dotnet', [
+//     'dev-certs',
+//     'https',
+//     '--export-path',
+//     certFilePath,
+//     '--format',
+//     'Pem',
+//     '--no-password',
+//   ], { stdio: 'inherit', }).status) {
+//     throw new Error("Could not create certificate.");
+//   }
+// }
 
 
 // https://vitejs.dev/config/
@@ -60,11 +60,11 @@ export default defineConfig(({ mode }) => {
           secure: false,
         },
       },
-      port: parseInt(env.VITE_PORT),
-      https: {
-        key: fs.readFileSync(keyFilePath),
-        cert: fs.readFileSync(certFilePath),
-      }
+      //port: parseInt(env.VITE_PORT),
+      // https: {
+      //   key: fs.readFileSync(keyFilePath),
+      //   cert: fs.readFileSync(certFilePath),
+      // }
     }
   }
 })
